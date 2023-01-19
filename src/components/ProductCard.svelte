c73524515748713947da26e545bc9f5399ff3d6f
revelcw <github@revelcw.com>
2023-01-19 10:43:38 -0500

    Ful commit + try to fix issue with wrong author

---
+++ src/components/ProductCard.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+  import Card from './Card.svelte';
+  import '../app.css';
+
+  export let title: string, description: string;
+</script>
+
+<Card>
+  <h1 class="text-3xl">{title}</h1>
+  <h2 class="text-[#BBBBBB] text-xl flex-1">
+    {description}
+  </h2>
+  <div class="flex flex-col gap-4 items-stretch justify-center w-full">
+    <slot />
+  </div>
+</Card>
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,32 @@
 module.exports = {
   content: ['./src/**/*.{html,js,svelte,ts}'],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        '2xl': { max: '1535px' },
+        // => @media (max-width: 1535px) { ... }
+
+        xl: { max: '1279px' },
+        // => @media (max-width: 1279px) { ... }
+
+        lg: { max: '1023px' },
+        // => @media (max-width: 1023px) { ... }
+
+        md: { max: '825px' },
+        // => @media (max-width: 767px) { ... }
+
+        sm: { max: '639px' },
+
+        xs: { max: '425px' },
+        // => @media (max-width: 375px) { ... }
+      },
+      colors: {
+        // Using modern `rgb`
+        background: '#19191C',
+        card: '#303033',
+        greyText: '#BBBBBB',
+      },
+    },
   },
   plugins: [],
-}
+};
